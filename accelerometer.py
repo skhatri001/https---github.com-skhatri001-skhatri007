5c0b92b0866c77235ffbec8b5b0e32bd8db1ecb6
skhatri007 <skhatri007@outlook.com>
2024-11-11 16:15:47 -0500

    Currently have the data format command (0x31) setup to output the highest resolution at +-2g of range. This corresponds to a resolution of 4mG/LSB. Will need to incorporate an offset for X,Y, and Z axis.

--- accelerometer.py
+++ accelerometer.py
@@ -24,7 +24,7 @@ def __init__(self, bus_num=1):
     self.bus.write_byte_data(ADXL345_I2C_ADDR, POWER_CTL, 0x08)
 
 byte = smbus.SMBus(bus_num).read_i2c_block_data(ADXL345_I2C_ADDR, 0x32, 6)
-format_command = int('10001000',2)
+format_command = int('00001000',2)
 print(type(format_command))
 data_format = smbus.SMBus(1).write_i2c_block_data(ADXL345_I2C_ADDR,0x31,[format_command]) # Need to insert class int at the 3rd output. hex(int()) will return a string which is not what we want. 
 print(smbus.SMBus(1).read_i2c_block_data(ADXL345_I2C_ADDR,0x31,1))
@@ -33,9 +33,9 @@ x = (byte[5] << 8 | byte[4])# Converts value into 16 bits.
 print(x)
 if x & (1 << 15):
     x = x - (1 << 16)
-    x = (x / (2**10)) * 2 * G
+    x = (x * (4e-3)) * 1 * G # When setting the max resolution = 4mG/LSB. 
 else:
-    x = (x / (2**10)) * 2 * G
+    x = (x * (4e-3)) * 1 * G
 
 power_mode = smbus.SMBus(bus_num).read_i2c_block_data(ADXL345_I2C_ADDR,0x2C,1)
 # print(hex(power_mode[0]))
